65a70f5b86254a9fa523b3a08fa514ef04efee12
AzimNazlen <mohamedazim@graduate.utm.my>
2022-06-14 22:52:07 +0800

    Update PerformanceReportsController.cs

--- Controllers/PerformanceReportsController.cs
+++ Controllers/PerformanceReportsController.cs
@@ -61,7 +61,8 @@ namespace MengajiOneToOneSystem.Controllers
         // GET: PerformanceReports/Create
         public ActionResult Create()
         {
-            var clients = db.Users
+            //var client2 = db.Users.Where(r => r.u_roles == "Pelajar");
+            var clients = db.Users.Where(r => r.u_roles == "Pelajar")
                 .Select(s => new
                 {
                     Text = s.u_id + " - " + s.u_fname,
