88beba600ced16ca9097ff8909edadb496b15902
norhafiyzha <hafiyzhahusni9@gmail.com>
2022-07-03 23:27:35 +0800

    Layout Update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -60,18 +60,5 @@ namespace MOTOSystem.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
