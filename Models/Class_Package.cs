3dde50a9b6420054fc40ff8e2bc7293727c2ee6b
AzimNazlen <mohamedazim@graduate.utm.my>
2022-07-04 00:05:24 +0800

    model

--- Models/Class_Package.cs
+++ Models/Class_Package.cs
@@ -11,9 +11,7 @@ namespace MOTOSystem.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class Class_Package
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,17 +19,11 @@ namespace MOTOSystem.Models
         {
             this.ClassRecords = new HashSet<ClassRecord>();
         }
-
-        [Required(ErrorMessage = "Ruangan ini perlu dipenuhi")]
-        [DisplayName("Pakej ID")]
+    
         public int cp_id { get; set; }
-        [Required(ErrorMessage = "Ruangan ini perlu dipenuhi")]
-        [DisplayName("Nama Pakej")]
         public string cp_name { get; set; }
-        [Required(ErrorMessage = "Ruangan ini perlu dipenuhi")]
-        [DisplayName("Yuran Pakej")]
         public Nullable<double> cp_fees { get; set; }
-
+    
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ClassRecord> ClassRecords { get; set; }
     }
--- Models/User.cs
+++ Models/User.cs
@@ -11,9 +11,7 @@ namespace MOTOSystem.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class User
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -23,42 +21,16 @@ namespace MOTOSystem.Models
             this.PerformanceReports = new HashSet<PerformanceReport>();
             this.SalaryInvoices = new HashSet<SalaryInvoice>();
         }
-
-        [Required(ErrorMessage = "Ruangan ini perlu dipenuhi")]
-        [DisplayName("ID Pengguna")]
+    
         public string u_id { get; set; }
-
-        [Required(ErrorMessage = "Ruangan ini perlu dipenuhi")]
-        [DataType(DataType.Password)]
-        [StringLength(150, ErrorMessage = "{0} hendaklah sekurang-kurangnya {2} karakter", MinimumLength = 5)]
-        [Display(Name = "Kata Laluan")]
         public string u_password { get; set; }
-
-        [Required(ErrorMessage = "Ruangan ini perlu dipenuhi")]
-        [Display(Name = "Email")]
-        [EmailAddress]
         public string u_email { get; set; }
-
-        [Required(ErrorMessage = "Ruangan ini perlu dipenuhi")]
-        [DisplayName("Nama Pertama")]
         public string u_fname { get; set; }
-
-        [Required(ErrorMessage = "Ruangan ini perlu dipenuhi")]
-        [DisplayName("Nama Akhir")]
         public string u_lname { get; set; }
-
-        [Required(ErrorMessage = "Ruangan ini perlu dipenuhi")]
-        [DisplayName("Jawatan")]
         public string u_roles { get; set; }
-
-        [Required(ErrorMessage = "Ruangan ini perlu dipenuhi")]
-        [DisplayName("No Telefon")]
         public string u_contact { get; set; }
         public string u_passcode { get; set; }
-
-        public string LoginErrorMessage { get; set; }
-        public bool RememberMe { get; set; }
-
+    
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ClassRecord> ClassRecords { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
